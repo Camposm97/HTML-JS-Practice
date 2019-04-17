99eef1ed354a99085c96d551730b31e9ebd481e2
Camposm97 <camposm1205@gmail.com>
2019-04-17 18:02:33 -0400

    Implemented other.html

---
+++ public_html/promptScript.js
@@ -0,0 +1,12 @@
+function submit() {
+    console.log("Adding \"Hello World\" to tag id: text1");
+    var tagP1 = document.getElementById("text1");
+    tagP1.
+    tagP1.innerHTML = "Hello World! " + tagP1.innerHTML;
+}
+
+function clear() {
+    console.log("Clearing text in tag id: text1");
+    var tagP1 = document.getElementById("text1");
+    tagP1.innerHTML = "";
+}
