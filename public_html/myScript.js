4f68dcd33f0ade1f75c327a5ed5ba3a026620f3b
Camposm97 <camposm1205@gmail.com>
2019-04-17 08:51:38 -0400

    First Commit

---
+++ public_html/myScript.js
@@ -0,0 +1,13 @@
+function functionY() {
+	console.log("Hello World");
+}
+
+function functionX() {
+	var x = 10;
+	var y = "x = ";
+	document.write(y + x);
+}
+
+function functionZ() {
+	
+}
---
+++ public_html/script.js
@@ -0,0 +1,17 @@
+function functionY() {
+	console.log("Hello World");
+}
+
+function functionX() {
+	var x = 10;
+	var y = "x = ";
+	document.write(y + x);
+}
+
+function myFunction() {
+	console.log("I've been clicked");
+}
+
+function functionZ(var name) {
+	console.log("name = " + name);
+}
